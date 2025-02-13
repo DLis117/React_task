8522aa67c1e1400a7c747899d874e96a6bffb65f
DLis117 <domills117@gmail.com>
2025-02-13 14:34:40 +0100

    last location done with navigate(-1)

--- myProject/src/SinglePost/SinglePost.tsx
+++ myProject/src/SinglePost/SinglePost.tsx
@@ -1,9 +1,7 @@
-import { useParams,useLocation,useNavigate } from "react-router";
+import { useParams,useNavigate } from "react-router";
 import { useQuery } from '@tanstack/react-query';
 import { useState } from "react";
 function SinglePost() {
-
-  const location = useLocation();
   const navigate = useNavigate();
   const { postId } = useParams();
   const [highlitedCommentId,setHighlitedCommentId] = useState();
@@ -14,14 +12,6 @@ function SinglePost() {
       fetch(`https://jsonplaceholder.typicode.com/posts/${postId}/comments`).then((res) => res.json()),
   });
 
-
-
-  const fromPage = location.state?.from || '/';
-
-  const handleGoBack = () => 
-  {
-    navigate(fromPage);
-  };
 
   if (isPending||isFetching) return <h1 className="text-3xl">Loading...</h1>;
   if (error) return <h1>{`An error has occurred: ${error.message}`}</h1>;
@@ -37,10 +27,10 @@ interface Comment {
 
   return (
     <>
-      <button onClick={handleGoBack} className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">Back</button>
+      <button onClick={()=>{navigate(-1)}} className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">Back</button>
       
           {data?.map((x)=>
-          <div key={x.id} className={highlitedCommentId===x.id?"mb-5 border-4 border-blue-500 inline-block cursor-pointer":"mb-5 cursor-pointer"} onClick={()=>{setHighlitedCommentId(x.id)}}>
+          <div key={x.id} className={highlitedCommentId===x.id?"mb-5 border-4 border-blue-500 cursor-pointer":"mb-5 cursor-pointer"} onClick={()=>{setHighlitedCommentId(x.id)}}>
               <p>comment id: {x.id}</p>
               <p>post id: {x.postId}</p>
               <p>email: {x.email}</p>
